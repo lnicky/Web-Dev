b718657d864bdc7f5f8a21eb17fa12fc894ae90a
Ted.SZ <tedsz@163.com>
2015-12-02 00:21:33 +0000

    layout

--- MovieTime/MovieTime/Models/Movie.cs
+++ MovieTime/MovieTime/Models/Movie.cs
@@ -16,7 +16,7 @@ namespace MovieTime.Models
         public string Title { get; set; }
 
         [Required(ErrorMessage = "{0} is required.")]
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
